591e8e7bd11b66979195847fd43c91168899d40c
Marcos  Oliveira <marcos.oliveira@fortbrasil.com.br>
2020-12-07 21:39:33 -0300

    finalizando page de dashboard no app

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -4,7 +4,7 @@ import api from '../services/api';
 
 interface AuthState {
     token: string;
-    user: Object;
+    user: User;
 }
 
 interface SignInCredentials{
@@ -12,8 +12,15 @@ interface SignInCredentials{
     password: string;
 }
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthContextData {
-    user: object;
+    user: User;
     signIn(credentials: SignInCredentials): Promise<void>;
     signOut(): void;
     loading: boolean;
@@ -33,6 +40,8 @@ export const AuthProvider: React.FC = ({ children }) => {
         ]);
 
         if(token[1] && user[1]){
+          api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
           setData({ token: token[1], user: JSON.parse(user[1]) }); 
         }
 
@@ -54,6 +63,8 @@ export const AuthProvider: React.FC = ({ children }) => {
           ['@GoBarber:token', token],
           ['@GoBarber:user', JSON.stringify(user)]
         ]);
+
+        api.defaults.headers.authorization = `Bearer ${token}`;
 
         setData({ token, user });
 
---
+++ src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const AppointmentCreated: React.FC = () => {
+  
+  return (
+    <View  />
+  );
+}
+
+export default AppointmentCreated;
